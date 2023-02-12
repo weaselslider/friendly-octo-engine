c54dfe31157fb62c886e99906f79f4486c2ea6c1
Jonathan Gunasekaran <jonathan.gunasekaran12@gmail.com>
2023-02-12 01:17:53 -0600

    edits to image loading and testing pygame in test.py

--- test.py
+++ test.py
@@ -1,2 +1,20 @@
 from sprites import *
+# import time
+from pygame.locals import *
 
+screen = pygame.display.set_mode((500, 500))
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == KEYDOWN:
+            if event.key == K_1:
+                a = Hazard()
+                screen.fill((175, 238, 238))
+                screen.blit(a.getImage(), (250, 250))
+                print(a.getName())
+
+    pygame.display.flip()
+pygame.quit()
